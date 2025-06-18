5ceae21ec76b1880fb75d4bee6cfec56aec4d4c7
Davi-reis21 <davidevreis@gmail.com>
2025-06-18 23:19:38 +0000

    API parte 2

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// Servir arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/', (req, res) =>{
-res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// Rota principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(3000, console.log('Rodando...'));
+// Iniciar o servidor
+app.listen(3000, () => console.log('Rodando na porta 3000...'));
