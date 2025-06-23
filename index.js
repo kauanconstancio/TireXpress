b529f6dacf2b907372e8270bda452db87aa75fc9
Davi-reis21 <davidevreis@gmail.com>
2025-06-23 23:46:51 +0000

    padrão API

--- index.js
+++ index.js
@@ -2,20 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Servir arquivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-// Rota principal
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-//rota de cadastro
-app.post('/cadastro', (req,res) => {
-console.log(req.body);
-res.send('Ok!');
-});
 
 // Iniciar o servidor
 app.listen(3000, () => console.log('Rodando na porta 3000...'));
