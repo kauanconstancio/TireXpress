e4fa1cec100c42c5bd56f1938201a108ace28ecd
Davi-reis21 <davidevreis@gmail.com>
2025-06-24 21:14:08 +0000

    Modificando API

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.get('/', (req ,res) => {
+res.sendFile(path.dirname('public', ))
+});
 
 // Iniciar o servidor
 app.listen(3000, () => console.log('Rodando na porta 3000...'));
