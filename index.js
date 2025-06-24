f921546fec7909e04f06aa2c4bb5e727c7304258
Davi-reis21 <davidevreis@gmail.com>
2025-06-24 21:29:20 +0000

    Test API

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get('/', (req ,res) => {
-res.sendFile(path.dirname('public', ))
+// Servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'pages', 'index.html'));
 });
 
-// Iniciar o servidor
 app.listen(3000, () => console.log('Rodando na porta 3000...'));
